f449060aba1a903441012d8e5ecd68be77f0b37c
chaOvO <ftt_zlc@163.com>
2021-03-24 18:25:57 +0800

    Initial commit

---
+++ src/main/java/com/hand/springboot02helloworldquick/SpringBoot02HelloworldQuickApplication.java
@@ -0,0 +1,13 @@
+package com.hand.springboot02helloworldquick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot02HelloworldQuickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot02HelloworldQuickApplication.class, args);
+    }
+
+}
